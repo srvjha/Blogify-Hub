436a165694ddb70afc8590bc980de475d6cf73d0
Saurav Jha <srvtj629@gmail.com>
2024-03-01 14:08:49 +0530

    bug fixed

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -3,7 +3,7 @@ import {Editor} from '@tinymce/tinymce-react'
 import {Controller} from 'react-hook-form'
 
 export default  function RTE({name,control,label,defaultValue=""}) {
-  const editorRef = useRef(null);
+  
   return (
     <div className='w-full'>
         {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
